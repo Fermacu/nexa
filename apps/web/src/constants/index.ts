c4413fcb4800cb515613fcedad716ff692d2bba1
Fernando Matamoros <fer.andres@hotmail.com>
2026-01-11 00:09:26 -0600

    Add constants for countries and industries
    Introduced COUNTRIES and INDUSTRIES constant arrays for use in forms and dropdowns, along with their respective TypeScript interfaces. Added a central index file to simplify imports of these constants throughout the application.

---
+++ apps/web/src/constants/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Constants Index
+ * 
+ * Central export point for all application constants.
+ * This makes it easy to import constants from a single location.
+ */
+
+export * from './countries'
+export * from './industries'
